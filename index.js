4538593e9541c9b5bb6e9fbfc9f006f074bc8ea2
Adrian McEwen <adrianm@mcqn.com>
2018-12-28 14:57:59 +0000

    Listen on port 80 rather than 3000

--- index.js
+++ index.js
@@ -51,6 +51,6 @@ app.get('/', function(req, res) {
         res.sendFile(__dirname+"/static_files/mqtt-socket.html")
 })
 
-http.listen(3000, function() {
-        console.log("listening on 3000")
+http.listen(80, function() {
+        console.log("listening on 80")
 })
